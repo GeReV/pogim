c42367d05884db659d1f0574915934d2e8bb05c4
Amir Grozki <amirgrozki@gmail.com>
2018-11-24 20:20:15 +0200

    Improved code a bit.

--- src/extract.py
+++ src/extract.py
@@ -1,101 +1,119 @@
 import numpy as np
 import cv2 as cv
+import sys
 from os import path
 
+
 def extract_regions(filename, img, contours, output_size=1200, pad=30):
-  for index, cnt in enumerate(contours):
-    (x, y, w, h) = cv.boundingRect(cnt)
-    top = y - pad
-    bottom = y + h + pad
-    left = x - pad
-    right = x + w + pad
+    for index, cnt in enumerate(contours):
+        (x, y, w, h) = cv.boundingRect(cnt)
+        top = y - pad
+        bottom = y + h + pad
+        left = x - pad
+        right = x + w + pad
 
-    region = img[top:bottom, left:right]
+        region = img[top:bottom, left:right]
 
-    moment = cv.moments(cnt)
-    cx = int(moment['m10']/moment['m00'])
-    cy = int(moment['m01']/moment['m00'])
+        (rh, rw, _) = region.shape
 
-    region_cx = (cx - x + pad)
-    region_cy = (cy - y + pad)
+        border_bottom = border_top = (output_size - rh) // 2
+        border_right = border_left = (output_size - rw) // 2
 
-    offset_x = (output_size//2) - region_cx
-    offset_y = (output_size//2) - region_cy
+        border_bottom += output_size - rh - border_bottom - border_top
 
-    (rh, rw, _) = region.shape
+        border_right += output_size - rw - border_left - border_right
 
-    M = np.float32([[1, 0, offset_x],[0, 1, offset_y]])
-    region = cv.warpAffine(region, M, (rw,rh), borderMode=cv.BORDER_CONSTANT, borderValue=(255, 255, 255))
+        region = cv.copyMakeBorder(
+            region,
+            border_top,
+            border_bottom,
+            border_left,
+            border_right,
+            cv.BORDER_CONSTANT,
+            value=(255, 255, 255)
+        )
 
-    border_top_bottom = (output_size - rh) // 2
-    border_left_right = (output_size - rw) // 2
+        out_filename = '{}{:04d}.jpg'.format(
+            path.splitext(path.basename(filename))[0], index)
 
-    region = cv.copyMakeBorder(
-      region, 
-      border_top_bottom, 
-      border_top_bottom, 
-      border_left_right, 
-      border_left_right, 
-      cv.BORDER_CONSTANT, 
-      value=(255, 255, 255)
+        cv.imwrite(out_filename, region)
+
+
+def extract_mask(img):
+    img = cv.medianBlur(img, 5)
+    lab = cv.cvtColor(img, cv.COLOR_BGR2Lab)
+
+    pad = 3
+
+    lower = np.array([128, 128 - pad, 128 - pad])
+    upper = np.array([255, 128 + pad, 128 + pad])
+
+    return cv.inRange(lab, lower, upper)
+
+
+def draw_debug(img, contours, pad=30):
+    moments = [cv.moments(cnt) for cnt in contours]
+    rects = [cv.boundingRect(cnt) for cnt in contours]
+
+    for (x, y, w, h) in rects:
+        cv.rectangle(img, (x, y), (x+w, y+h), (127, 127, 255))
+        cv.rectangle(img, (x - pad, y - pad), (x+w+pad, y+h+pad), (255, 0, 0))
+
+    for moment in moments:
+        cx = int(moment['m10']/moment['m00'])
+        cy = int(moment['m01']/moment['m00'])
+        cv.circle(img, (cx, cy), 2, (0, 0, 255), 2)
+
+    img = cv.drawContours(img, contours, -1, (128, 64, 0), 1)
+
+    img = cv.resize(img, None, fx=0.1, fy=0.1)
+
+    cv.imshow('image', img)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+
+def extract_pogs(filename):
+    img = cv.imread(filename, cv.IMREAD_COLOR)
+
+    mask = extract_mask(img)
+
+    inv_mask = cv.bitwise_not(mask)
+
+    image, contours, hierarchy = cv.findContours(
+        inv_mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+    contours = filter(
+        lambda cnt: cv.contourArea(cnt) > (img.size // 500),
+        contours
     )
 
-    out_filename = '{}{:04d}.jpg'.format(path.splitext(path.basename(filename))[0], index)
+    print("Extracting {} Pogs...".format(len(contours)))
 
-    cv.imwrite(out_filename, region)
+    pad = img.shape[0] // 180
+
+    extract_regions(filename, img, contours, pad=pad)
+
+    print("Done.\n")
+
+    # draw_debug(img, contours, pad=pad)
+
 
 def main():
-  filename = '../assets/pogim_2.jpg'
+    if len(sys.argv) == 1:
+        print("Usage: {} FILE [...FILE]".format(sys.argv[0]))
+        exit(1)
 
-  img = cv.imread(filename, cv.IMREAD_COLOR)
+    for filename in sys.argv[1:]:
+        print("Processing: {}".format(path.abspath(filename)))
+        extract_pogs(filename)
 
-  # img = cv.resize(img, None, fx=0.2, fy=0.2)
+    # max_width = max(rects, key=lambda x: x[2])[2] + pad * 2
+    # max_height = max(rects, key=lambda x: x[3])[3] + pad * 2
 
-  hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    # print(max_width)
+    # print(max_height)
 
-  lower_white = np.array([0,0,215])
-  upper_white = np.array([360,15,255])
-
-  # Threshold the HSV image to get only blue colors
-  mask = cv.inRange(hsv, lower_white, upper_white)
-
-  inv_mask = cv.bitwise_not(mask)
-
-  image, contours, hierarchy = cv.findContours(inv_mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
-
-  contours = filter(lambda cnt: cv.contourArea(cnt) > 500, contours)
-
-  pad = 60
-
-  extract_regions(filename, img, contours, pad=pad)
-  
-  moments = [cv.moments(cnt) for cnt in contours]
-  rects = [cv.boundingRect(cnt) for cnt in contours]
-
-
-  for (x, y, w, h) in rects:
-    cv.rectangle(img, (x, y), (x+w, y+h), (127, 127, 255))
-    cv.rectangle(img, (x - pad, y - pad), (x+w+pad, y+h+pad), (255, 0, 0))
-
-  for moment in moments:
-    cx = int(moment['m10']/moment['m00'])
-    cy = int(moment['m01']/moment['m00'])
-    cv.circle(img, (cx, cy), 2, (0, 0, 255), 2)
-
-  img = cv.drawContours(img, contours, -1, (128, 64, 0), 1)
-  
-  cv.imshow('image', img)
-  cv.waitKey(0)
-  cv.destroyAllWindows()
-
-  max_width = max(rects, key=lambda x:x[2])[2] + pad * 2
-  max_height = max(rects, key=lambda x:x[3])[3] + pad * 2
-
-  print(max_width)
-  print(max_height)
-
-
-    
 
 if __name__ == '__main__':
-  main()
+    main()
