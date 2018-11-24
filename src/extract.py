fe7691825c28774b190269a0ade2235358d55d3a
Amir Grozki <amirgrozki@gmail.com>
2018-11-25 00:08:46 +0200

    Cleaned up code.

--- src/extract.py
+++ src/extract.py
@@ -4,7 +4,7 @@ import sys
 from os import path
 
 
-def extract_regions(filename, img, contours, output_size=1200, pad=30):
+def extract_regions(filename, img, contours, pad=30):
     for index, cnt in enumerate(contours):
         (x, y, w, h) = cv.boundingRect(cnt)
         top = y - pad
@@ -14,26 +14,7 @@ def extract_regions(filename, img, contours, output_size=1200, pad=30):
 
         region = img[top:bottom, left:right]
 
-        (rh, rw, _) = region.shape
-
-        border_bottom = border_top = (output_size - rh) // 2
-        border_right = border_left = (output_size - rw) // 2
-
-        border_bottom += output_size - rh - border_bottom - border_top
-
-        border_right += output_size - rw - border_left - border_right
-
-        region = cv.copyMakeBorder(
-            region,
-            border_top,
-            border_bottom,
-            border_left,
-            border_right,
-            cv.BORDER_CONSTANT,
-            value=(255, 255, 255)
-        )
-
-        out_filename = '{}{:04d}.jpg'.format(
+        out_filename = '{}_{:04d}.jpg'.format(
             path.splitext(path.basename(filename))[0], index)
 
         cv.imwrite(out_filename, region)
@@ -52,17 +33,11 @@ def extract_mask(img):
 
 
 def draw_debug(img, contours, pad=30):
-    moments = [cv.moments(cnt) for cnt in contours]
     rects = [cv.boundingRect(cnt) for cnt in contours]
 
     for (x, y, w, h) in rects:
         cv.rectangle(img, (x, y), (x+w, y+h), (127, 127, 255))
         cv.rectangle(img, (x - pad, y - pad), (x+w+pad, y+h+pad), (255, 0, 0))
-
-    for moment in moments:
-        cx = int(moment['m10']/moment['m00'])
-        cy = int(moment['m01']/moment['m00'])
-        cv.circle(img, (cx, cy), 2, (0, 0, 255), 2)
 
     img = cv.drawContours(img, contours, -1, (128, 64, 0), 1)
 
@@ -107,12 +82,6 @@ def main():
     for filename in sys.argv[1:]:
         print("Processing: {}".format(path.abspath(filename)))
         extract_pogs(filename)
-
-    # max_width = max(rects, key=lambda x: x[2])[2] + pad * 2
-    # max_height = max(rects, key=lambda x: x[3])[3] + pad * 2
-
-    # print(max_width)
-    # print(max_height)
 
 
 if __name__ == '__main__':
