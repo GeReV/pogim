6461d58bcc7694fe1f41602a6b76c2ebb837085f
Amir Grozki <amirgrozki@gmail.com>
2019-03-09 04:06:09 +0200

    Updated deploy scripts.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -22,7 +22,7 @@ echo "Removing existing files"
 rm -rf public/*
 
 echo "Generating site"
-hugo
+yarn build
 
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (deploy.sh)"
