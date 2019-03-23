07bb1d37d8de622b2a23f587d57d909a1495cfa1
Amir Grozki <amirgrozki@gmail.com>
2019-03-23 02:53:35 +0200

    Added merge_toml.py script.

---
+++ scripts/merge_toml.py
@@ -0,0 +1,48 @@
+import sys
+
+import toml
+
+
+def merge(files):
+    item_lists = []
+
+    result_list = []
+
+    number_set = set()
+
+    for file in files:
+        with open(file, 'rb') as toml_file:
+            data = toml.load(toml_file)
+
+            item_lists.append(data['items'])
+
+    for items in item_lists:
+        for item in items:
+            if item['number'] in number_set:
+                continue
+
+            number_set.add(item['number'])
+
+            result_list.append(item)
+
+    result_list = sorted(result_list, key=lambda x: x['number'])
+
+    output_dict = {
+        "items": result_list
+    }
+
+    with open('merged.toml', 'wb') as toml_file:
+        toml.dump(output_dict, toml_file)
+
+
+def main():
+    if len(sys.argv) < 3:
+        print("Usage: {} FILE FILE".format(sys.argv[0]))
+        exit(1)
+
+    print("Merging...")
+    merge(sys.argv[1:])
+
+
+if __name__ == '__main__':
+    main()
