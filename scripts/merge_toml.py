28e9acf02383230919cfbfd91027eae626be3478
Amir Grozki <amirgrozki@gmail.com>
2019-06-22 03:16:10 +0300

    merge_toml.py will now fill in missing items.

--- scripts/merge_toml.py
+++ scripts/merge_toml.py
@@ -3,7 +3,16 @@ import sys
 import toml
 
 
+def irange(start, end):
+    return range(start, end + 1)
+
+
 def merge(files):
+    base_list = set(irange(1, 1030))
+    base_list = base_list.difference(irange(541, 600))
+    base_list = base_list.difference(irange(661, 720))
+    base_list = base_list.difference(irange(841, 1000))
+
     item_lists = []
 
     result_list = []
@@ -11,27 +20,39 @@ def merge(files):
     number_set = set()
 
     for file in files:
-        with open(file, 'rt') as toml_file:
+        with open(file, "rt") as toml_file:
             data = toml.load(toml_file)
 
-            item_lists.append(data['items'])
+            item_lists.append(data["items"])
 
     for items in item_lists:
         for item in items:
-            if item['number'] in number_set:
+            if item["number"] in number_set and item["missing"] != True:
                 continue
 
-            number_set.add(item['number'])
+            number_set.add(item["number"])
 
             result_list.append(item)
 
-    result_list = sorted(result_list, key=lambda x: x['number'])
+    missing_numbers = base_list.difference(number_set)
+
+    result_list.extend(map(lambda x: {
+        "number": x,
+        "series": "missing",
+        "backface": "default",
+        "preview": "missing.svg",
+        "shiny": False,
+        "original": "missing.svg",
+        "missing": True,
+    }, missing_numbers))
+
+    result_list = sorted(result_list, key=lambda x: x["number"])
 
     output_dict = {
         "items": result_list
     }
 
-    with open('merged.toml', 'wt') as toml_file:
+    with open("merged.toml", "wt") as toml_file:
         toml.dump(output_dict, toml_file)
 
 
@@ -44,5 +65,5 @@ def main():
     merge(sys.argv[1:])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
